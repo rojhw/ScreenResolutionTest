da07ae93d5609589673286e33907bcc6df41d785
rojhw <unity3013@gmail.com>
2017-06-21 15:45:37 +0900

    firstCommit

---
+++ app/src/main/java/com/example/screenresolutiontest/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.screenresolutiontest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webview = (WebView)findViewById(R.id.webview);
+        webview.loadUrl("file:///android_asset/error_connection.html");
+    }
+}
